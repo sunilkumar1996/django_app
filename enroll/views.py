f5bf485804cc1c110353bb9ae415c5880ae6f6af
sunilkumar1996 <sunilrajputdev@gmail.com>
2020-08-21 13:09:10 +0530

    login and logout application

---
+++ enroll/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, HttpResponseRedirect
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+
+
+# Signup view Function
+def sign_up(request):
+    if request.method == 'POST':
+        # form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Account Created Successfully !!')
+            form.save()
+    else:
+        # form = UserCreationForm()
+        form = SignUpForm()
+    return render(request, 'enroll/signup.html', {'form': form})
+
+
+# Login view Function here
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                uname = form.cleaned_data['username']
+                upass = form.cleaned_data['password']
+                user = authenticate(username=uname, password=upass)
+                if user is not None:
+                    messages.success(request, 'Logged is Successfull !!')
+                    login(request, user)
+                    return HttpResponseRedirect('/profile/')
+        else:
+            form = AuthenticationForm()
+        return render(request, 'enroll/userlogin.html', {'form': form})
+    else:
+        return HttpResponseRedirect('/profile/')
+
+
+# Profile view function here
+def user_profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'enroll/profile.html', {'name': request.user})
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+# Logout view function here.
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')
